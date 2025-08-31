5aeebeeac24d2f3810fc151ce69aaa5ca49934b5
aryanphulkar90 <aryanphulkar1@gmail.com>
2025-08-31 22:22:15 +0530

    minor auth middleware fix

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -26,7 +26,7 @@ const userAuth = async(req, res, next) => {
 
     next();
   } catch (err) {
-    res.status(400).send("Error " + err);
+    return res.status(400).send("Error " + err);
   }
 };
 
